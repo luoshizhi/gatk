74fe5fb0d46e7820066a8a48fa67c6c87ee9368c
Steve Huang <shuang@broadinstitute.org>
2017-11-22 00:14:15 -0500

    fix bug in incomplete picture from multiple alignmentl; also improved documentation

--- src/main/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/prototype/AssemblyContigWithFineTunedAlignments.java
+++ src/main/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/prototype/AssemblyContigWithFineTunedAlignments.java
@@ -117,37 +117,70 @@ final class AssemblyContigWithFineTunedAlignments {
 
     //==================================================================================================================
 
+    /**
+     * This predicate tests if an assembly contig has the full event (i.e. alt haplotype) assembled
+     * Of course, the grand problem of SV is always not getting the big-enough picture
+     * but here we have a more workable definition of what is definitely not big-enough:
+     *
+     * If the assembly contig, with its (picked) alignments, shows any of the following signature,
+     * it is definitely not giving the whole picture of the alt haplotype,
+     * hence without other types of evidence (or linking breakpoints, which itself needs other evidence anyway),
+     * human-friendly interpretation for them is unreliable.
+     * <ul>
+     *     <li>
+     *         head and tail alignment contain each other in terms of their reference span
+     *     </li>
+     *     <li>
+     *         head and tail alignment mapped to different chromosome
+     *     </li>
+     *     <li>
+     *         head and tail alignment mapped to different strands
+     *     </li>
+     *     <li>
+     *         head and tail alignment have reference order switch, indicating we have likely
+     *         assembled across a complicated tandem duplication breakpoint
+     *     </li>
+     *     <li>
+     *         valid region, as defined by the region bounded by the outer most boundaries of head and tail alignments,
+     *         overlaps with any of the middle alignments but does not completely contain them
+     *         (note that middle alignments completely disjoint from the valid region are OK,
+     *          they indicate remote parts of reference being duplicated and inserted between head/tail)
+     *     </li>
+     * </ul>
+     * In summary, the assembly contig should "resume the normal flow" as defined by the reference,
+     * even though we allow the flow to be interrupted in the middle.
+     * This is similar to the case where a particular mapping is unreliable without confident left and right flanking.
+     */
     private boolean hasIncompletePictureFromMultipleAlignments() {
         final int goodAlignmentsCount = contig.alignmentIntervals.size();
-        Utils.validateArg(goodAlignmentsCount > 2,
-                "assumption that input contig has more than 2 alignments is violated.\n" +
-                        contig.toString());
 
         final AlignmentInterval head = contig.alignmentIntervals.get(0),
                                 tail = contig.alignmentIntervals.get(goodAlignmentsCount-1);
 
-        // tail not resuming the "direction of flow" started by head
+        // mapped to different chr or strand
         if ( !head.referenceSpan.getContig().equals(tail.referenceSpan.getContig()) || head.forwardStrand != tail.forwardStrand)
             return true;
 
-        // reference span anomaly
         final SimpleInterval referenceSpanHead = head.referenceSpan,
                              referenceSpanTail = tail.referenceSpan;
+
+        // head or tail's ref span contained in the other's
         if (referenceSpanHead.contains(referenceSpanTail) || referenceSpanTail.contains(referenceSpanHead))
             return true;
 
-        final boolean refSpanAnomaly =
+        // middle alignments' ref span should be either 1) disjoint from valid region, or 2) completely contained in valid region
+        final SimpleInterval validRegion = new SimpleInterval(referenceSpanHead.getContig(),
+                                                              Math.min(referenceSpanHead.getStart(), referenceSpanTail.getStart()),
+                                                              Math.max(referenceSpanHead.getEnd(), referenceSpanTail.getEnd()));
+        final boolean notCompleteDupRegion =
                 contig.alignmentIntervals
-                        .subList(1, goodAlignmentsCount - 1)
+                        .subList(1, goodAlignmentsCount - 1) // take middle (no head no tail) alignments' ref span
                         .stream().map(ai -> ai.referenceSpan)
-                        .anyMatch( middle -> { // middle alignments' ref span either disjoint from head/tail, or completely contained in head/tail ref span
-                            final boolean badHead = middle.overlaps(referenceSpanHead) && !referenceSpanHead.contains(middle);
-                            final boolean badTail = middle.overlaps(referenceSpanTail) && !referenceSpanTail.contains(middle);
-                            return badHead || badTail;
-                        });
-        if (refSpanAnomaly)
+                        .anyMatch( middle -> middle.overlaps(validRegion) && !validRegion.contains(middle));
+        if (notCompleteDupRegion)
             return true;
 
+        // reference order switch, indicating a complicated tandem duplication breakpoint
         if (head.forwardStrand) {
             return referenceSpanHead.getStart() >= referenceSpanTail.getStart();
         } else {
